96f4e574a5dc023e183e14b16a84c12051d91786
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-09-20 04:34:05 +0000

    lab4 done

--- lab4/lab4_code.asm
+++ lab4/lab4_code.asm
@@ -11,12 +11,12 @@
 ; R4 contains counter for inner loop
 ; R5 contains current sum
 
-            x3000
+	    .ORIG x3000		      ;*(.ORIG was missing)* - program starts at location x3000	
 
             LD    R1,INPUT            ; R1 contains input number
-            LD    R2,x3010            ; R2 contains -1
+            LD    R2,NEGONE           ; R2 contains -1				*(have used a label here)*
             ADD   R3,R1,R2            ; R3 contains input number -1
-            ADD   R3,,R3,R2           ; R3 contains input number -2
+            ADD   R3,R3,R2            ; R3 contains input number -2		*(there were 2 commas, only one was required)*
                                       ;   (initializes outer count)
 OUTERLOOP   ADD   R4,R0,R3            ; Copy outer count into inner count
 
@@ -27,16 +27,17 @@ INNERLOOP   ADD   R5,R5,R1            ; Increment sum
             BRzp  INNERLOOP           ; Branch to inner loop if inner count
                                       ;   is positive or zero
             ADD   R1,R0,R5            ; R1 now contains sum result from inner loop
-            LD    R5,x300F            ; Clear R5 (previous sum) to 0
+            LD    R5,ZERO             ; Clear R5 (previous sum) to 0		*(have used a label here in place of x300F)*
             ADD   R3,R3,R2            ; Decrement outer count
-            BRpz  OUTERLOOP           ; Branch to outer loop if outer count
+            BRzp  OUTERLOOP           ; Branch to outer loop if outer count	*(z and p had been interchanged)*
                                       ;   is positive or zero
 
-            ST   R1,x3011             ; This address contains X!          
+            ST   R1,STORERES             ; This address contains X!          
             TRAP x25
 
 INPUT      .FILL  x0005               ; Input for X!, in this case X = 5
-           .FILL  x0000
-           .FILL  xFFFF               ; 2's complement of 1 (i.e. -1)
-           .FILL  x0000               ; At program completion, the result is
+ZERO	   .FILL  x0000		      ; *(have added a label here)* 	
+NEGONE     .FILL  xFFFF               ; 2's complement of 1 (i.e. -1)		*(have added a label here)*
+STORERES   .FILL  x0000               ; At program completion, the result is
                                       ;   stored here
+.END
