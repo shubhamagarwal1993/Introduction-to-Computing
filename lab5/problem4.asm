2f4595ba3dcc6a02712a57526872f0a4be5293a0
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-09-30 07:12:21 +0000

    lab5

--- lab5/problem4.asm
+++ lab5/problem4.asm
@@ -1,1 +1,25 @@
 ;Answer problem 4 here
+
+ .ORIG x3000
+       AND   R1,R1,#0
+       ADD   R1,R1,#5
+       LD    R2,NUM
+       LD    R3,STORE
+       AND   R6,R6,#0
+       ADD   R6,R6,#4
+       NOT   R1,R1
+       ADD   R1,R1,#1
+LOOP   LDR   R5,R2,#0
+       ADD   R5,R5,R1
+       BRp   BREAK
+       ADD   R2,R2,#0
+       ADD   R6,R6,#-1
+       BRp   LOOP
+       TRAP  x25
+BREAK  LDR   R5,R2,#0
+       STR   R5,R3,#0
+       ADD   R3,R3,#1
+       TRAP  x25
+NUM    .FILL x40A0
+STORE  .FILL x40AA
+       .END
