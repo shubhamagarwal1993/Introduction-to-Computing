5fce83e455e465ad634e4e9e34d5408636e5adad
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-09-30 07:12:21 +0000

    lab5

--- sagarwl4/lab5/problem6.asm
+++ sagarwl4/lab5/problem6.asm
@@ -1,1 +1,27 @@
 ;Answer problem 6 here
+.ORIG x3000
+        LDI   R1,NUM
+        LD    R2,ASIZE
+        LD    R3,BSIZE
+        AND   R6,R6,#0
+        ADD   R6,R6,R3
+        ADD   R3,R3,R1
+        ADD   R4,R4,R1
+        ADD   R4,R4,R1
+        AND   R7,R7,#0
+        ADD   R1,R6,#-1
+LOOP    LDR   R5,R2,#0
+        ADD   R7,R5,R7
+        LDR   R5,R3,#0
+        ADD   R7,R7,R5
+        STR   R4,R7,#0
+        ADD   R4,R4,#1
+        ADD   R3,R3,#1
+        ADD   R2,R2,#1
+        ADD   R1,R1,#-1
+        BRp   LOOP
+        TRAP  x25
+NUM     .FILL x4000
+ASIZE   .FILL x4001
+BSIZE   .FILL x4001
+        .END
