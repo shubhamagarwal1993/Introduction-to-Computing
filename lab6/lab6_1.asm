32935beeea501ceac41271e2e3985a2075c05ff0
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-10-04 21:03:13 +0000

    lab 6 1st part done

--- lab6/lab6_1.asm
+++ lab6/lab6_1.asm
@@ -1,1 +1,31 @@
-.ORIG x3000
+.ORIG x3000				;code starts here
+
+;PURPOSE OF REGISTERS
+;R0 - used to pass parameter 'n' into to the subroutine
+;R1 - 
+;R2 - 
+;R3 -
+;R4 - 
+;R5 - 
+;R6 - 
+;R7 - 
+
+
+
+
+		LD R0,			;load R0 with value to be passed into the subroutine
+		JSR SUB			;jump to the subroutine
+			
+		
+		
+		
+		
+		
+		
+		
+		HALT			;TRAPx25
+SUB
+
+
+
+						 
