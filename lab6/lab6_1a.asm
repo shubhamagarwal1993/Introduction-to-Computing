b2d8247a27c2def30b1192bc9c5c0ccecf5ec29a
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-10-05 21:48:48 +0000

    lab6 done

--- lab6/lab6_1a.asm
+++ lab6/lab6_1a.asm
@@ -1,1 +1,42 @@
-.ORIG x3000
+.ORIG x3000				;code starts here
+
+;PURPOSE OF REGISTERS
+;R0 - used to pass parameter 'n' into to the subroutine
+;R1 - used to pass the return value f(n) from sub to prog
+;R2 - used as a counter for adding in the subroutine
+;R4 - stores the final result (also in R1 , R4 is just a copy of the answer)
+
+
+;CLEARING ALL THE REGISTERS
+		AND R0,R0,#0		;clear
+		AND R1,R1,#0		;clear
+		AND R2,R2,#0		;clear
+		AND R3,R3,#0		;clear
+		AND R4,R4,#0		;clear
+		AND R5,R5,#0		;clear
+		AND R6,R6,#0		;clear
+
+;CODE EXECUTION
+
+		LD R0, N		;load R0 with value to be passed into the subroutine
+		STI R7,SaveReg		;we save the value of R7 		
+		JSR SUB			;jump to the subroutine
+		
+		ADD R4,R4,R1		;we load the value of R1 into R4 to see if STI WORKS		
+
+		HALT			;TRAPx25
+SUB		
+		ADD R2,R2,#9		;counter
+loop1		ADD R1,R1,R0		;load R1 with N
+		ADD R2,R2,#-1		;decrement counter
+		BRp loop1		;go to loop1 if R2 is positive
+		ADD R1,R1,#15		;load R1 with 16
+		ADD R1,R1,#1		;instruction continued from above		
+		RET			;loads saved value of R7 into the PC
+
+N		.FILL #2		;
+SaveReg		.FILL x0000		;
+RESULT		.FILL x0000		;
+
+		.END
+						 
