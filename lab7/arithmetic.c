50394c519ea19d1e37f0e76883771b132ca87d22
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-10-10 06:51:20 +0000

    lab7 done

---
+++ lab7/arithmetic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+	{
+	int a;
+	int b;
+	printf("enter 2 integers");
+	scanf("%d %d", &a, &b);
+	printf("a+b = %d\n", a+b);
+	printf("a-b = %d\n", a-b);
+	printf("a*b = %d\n", a*b);
+	return 0;
+	}
+
