638460083239ed80c460423e939dee7a42be4dec
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-10-10 06:40:20 +0000

    lab7 done

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -2,18 +2,22 @@
    The series depends on the value of the variable a which is 256 in this case.
    Hence in this case the series looks like 256,16,4,2,1. */
 
+#include <stdio.h>
+#include <math.h>
 int main()
 {
     int a = 256;
-    printf("%d\n", a); // Print initial value of a
+    int square_root_a;
+    printf("%d\n", a); // Print initial value of a 
 
 repeat:
     square_root_a = sqrt(a);
     if (square_root_a >= 2)
     {
         printf("%d\n", square_root_a);
-        a = square_root_a
+        a = square_root_a;
         goto repeat; // Go back to label "repeat" to keep iterating
+    }	
     else
         printf("%d\n", 1); // Print 1 to complete the series
     
