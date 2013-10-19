67ed9a9fb9e9a4f2aec9b324866e3374b6943a6e
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-10-19 18:09:22 +0000

    mp2.2 almost there

--- sagarwl4/mp2.2/getsize.asm
+++ sagarwl4/mp2.2/getsize.asm
@@ -2,11 +2,68 @@
 ; Comments
 .ORIG x3100
 
+;SAVE ALL THE REGISTERS  
+		ST R0,SAVE_0		;
+		ST R1,SAVE_1		;
+		ST R6,SAVE_6		;
+		ST R7,SAVE_7		;
 
 
+;PURPOSE OF REGISTERS
+;R0 - will be used for all the input and output.
+;R1 - copy of R0 where all the code will be executed.
+;R6 - when the size is valid, the size is stored in R6. 
+;R7 - has the PC value of the next instruction after this subroutine returns. 
 
 
+;CLEAR REGISTERS
+		AND R0,R0,#0		;clear
+		AND R1,R1,#0		;clear 
+		AND R6,R6,#0		;clear
+				
+;CODE EXECUTION
+LOOP_ST		LEA R0,ASKSIZE		;asks the size from the user
+		PUTS			;calls a system trap routine which outputs string
+		GETC			;place ASCII value of input character into R1
 
-ASKSIZE	.STRINGZ "\nPlease enter the size of your board[2..9]:\n" ; The words that should be displayed to ask size
-ERRORSIZE .STRINGZ "\nYour input is not valid!\n"	; The words that should be displayed if the input is not valid
+		ADD R1,R1,R0
+		OUT
+		
+;GETTING ACTUAL VALUE IN ASCII
+		ADD R1,R1,#-15		;add 48 (x0030) to the input ASCII 
+		ADD R1,R1,#-15		;
+		ADD R1,R1,#-15		;R1 now contains the actual value	
+		ADD R1,R1,#-3
+
+;CHECKING FOR VALID INPUT
+		ADD R1,R1,#-2		;checking for lower limit.
+		BRn INVALID		;
+		BRzp UPPER_LIMIT	;	
+		
+INVALID		LEA R0,ERRORSIZE	;
+		PUTS
+		BRnzp LOOP_ST		;if the input is invalid, then ask for input again
+
+UPPER_LIMIT	ADD R1,R1,#2		;get original value
+		ADD R1,R1,#-9		;
+		BRp INVALID		
+		BRnz FINAL
+
+FINAL		ADD R1,R1,#9		;input number
+
+		ADD R6,R6,R1		;loads the final valid input into R6
+
+
+;LOAD ALL THE REGISTERS TO THEIR INITIAL VALUE SO THAT NO VALUE HAS CHANGED AFTER THIS SUBROUTINE
+		LD R0,SAVE_0		;load from x3150
+		LD R1,SAVE_1		;load from x3151
+		LD R6,SAVE_6		;load from x3152
+		LD R7,SAVE_7		;load from x3153
+		RET
+ASKSIZE		.STRINGZ "\nPlease enter the size of your board[2..9]:\n" ; Words displayed to ask size
+ERRORSIZE 	.STRINGZ "\nYour input is not valid!\n"	; Words that should be displayed if input is not valid
+SAVE_0		.BLKW 1
+SAVE_1		.BLKW 1
+SAVE_6		.BLKW 1
+SAVE_7		.BLKW 1
 .END
--- sagarwl4/mp2.2/printboard.asm
+++ sagarwl4/mp2.2/printboard.asm
@@ -2,6 +2,81 @@
 ; Comments
 .ORIG x3200
 
+;SAVE ALL THE REGISTERS since we dont want the value of registers to be lost when this subroutine works 
+		ST R0,SAVE_0		;
+		ST R1,SAVE_1		;
+		ST R2,SAVE_2		;
+		ST R3,SAVE_3		;
+		ST R6,SAVE_6		;
+		ST R7,SAVE_7		;
 
+;PURPOSE OF REGISTERS
+;R0 - will be used for all the input and output
+;R1 - has the starting location of the gameboard
+;R2 - will be used as a counter for the row
+;R3 - will be used as a counter for the column
+;R6 - contains the size of the game board 
+ 
+
+
+;CLEAR REGISTERS
+		AND R0,R0,#0		;clear
+		AND R1,R1,#0		;clear 
+		AND R2,R2,#0		;clear 
+		AND R3,R3,#0		;clear 
+		AND R6,R6,#0		;clear
+		
+		ADD R6,R6,#3
+
+;add a blank line
+		LEA R0,NEXT_LINE
+		PUTS
+		
+	
+;CODE EXECUTOIN 
+		LD R1,GAMEBOARD
+		ADD R3,R3,R6		;put the input number in R3
+		
+		
+
+LINE_AGAIN	ADD R2,R2,R6		;put the input number in R2
+LOOP1		ADD R1,R1,#1
+		LDR R0,R1,#0
+		OUT
+		
+		ADD R2,R2,#-1		;acts as a counter for columns
+		BRnz BREAK_LINE
+		BRp LOOP1
+
+BREAK_LINE	LEA R0,NEXT_LINE
+		PUTS
+		ADD R3,R3,#-1		;acts as a counter for rows
+		BRnz STOP
+		BRp LINE_AGAIN
+
+
+;add a blank line
+STOP		LEA R0,NEXT_LINE		
+		PUTS
+
+;LOAD ALL THE REGISTERS TO THEIR INITIAL VALUE SO THAT NO VALUE HAS CHANGED AFTER THIS SUBROUTINE
+		LD R0,SAVE_0		;load
+		LD R1,SAVE_1		;load
+		LD R2,SAVE_2		;load
+		LD R3,SAVE_3 		;load
+		LD R6,SAVE_6		;load
+		LD R7,SAVE_7		;load
+
+		RET
+
+SAVE_0		.BLKW 1
+SAVE_1		.BLKW 1
+SAVE_2		.BLKW 1
+SAVE_3		.BLKW 1
+SAVE_6		.BLKW 1
+SAVE_7		.BLKW 1
+
+NEXT_LINE	.STRINGZ "\n"
+GAMEBOARD	.FILL x35FF
 
 .END
