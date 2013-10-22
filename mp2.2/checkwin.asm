4394d76781e3668c05bb3e2417aa35f307f7e809
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-10-22 23:17:14 +0000

    mp2.2 check

--- mp2.2/checkwin.asm
+++ mp2.2/checkwin.asm
@@ -1,16 +1,16 @@
 ; This file should be submitted in mp2.2
 ; Comments
 ;PURPOSE OF REGISTERS
-;R0 - 
-;R1 - 
-;R2 - 
+;R0 - checks if all the pieces are 0 or not (output)
+;R1 - spare
+;R2 - spare
 ;R3 - contains (R6)^2
 ;R4 - is the counter
-;R5 - 
-;R6 - 
+;R5 - is a counter for decrementing (size)^2 
+;R6 - contains game board size(not to be touched)
 
 .ORIG x3500
-;SAVE ALL THE REGISTERS
+;------------------SAVE ALL THE REGISTERS--------------------------
 			ST R1,SAVE_1
 			ST R2,SAVE_2
 			ST R3,SAVE_3
@@ -18,7 +18,9 @@
 			ST R5,SAVE_5
 			ST R7,SAVE_7
 
-;CLEAR ALL REGISTERS
+;-----------------CLEAR ALL REGISTERS------------------------------
+			AND R0,R0,#0		;clear	
+			ADD R1,R1,#1		;we add 1 to R0, assuming the board is not flipped		
 			AND R1,R1,#0		;clear
 			AND R2,R2,#0		;clear
 			AND R3,R3,#0		;clear
@@ -28,9 +30,10 @@
 ;----------------making a counter-------------------------------------------------------------
 			ADD R5,R6,#0		;R5 now has the size
 		
-LOOP2		ADD R3,R6,#0		;R3 now has the square of R6(size^2)
+LOOP2			ADD R3,R3,R6		;R3 now has the square of R6(size^2)
+			ADD R3,R3,#-1		;we not have (size^2)-1 because we check x3600 outside the loop
 			ADD R5,R5,#-1		;		
-			BRp LOOP2			;R5 is a loop counter		
+			BRp LOOP2		;R5 is a loop counter		
 					
 ;---------------------------------------------------------------------------------------------
 			AND R5,R5,#0		;clear - now we have R1,R2,R4,R5,R7 FREE
@@ -39,27 +42,33 @@ LOOP2		ADD R3,R6,#0		;R3 now has the square of R6(size^2)
 			ADD R2,R2,#-1		;load R2 with -1
 			
 		
-LOOP1		LDI R4,GAMEBOARD	;load the value stored in x3600
+			LDI R4,GAMEBOARD	;load the value stored in x3600
 			ADD R3,R4,R2		;we see if the result is -ve or 0
-			BRn NEXT			;if it is negative, decrement the counter		
-			BRzp STOP			;if it is 1, we go to halt straight
-		
-NEXT		ADD R1,R1,#1		;checks the next value
-			ADD R3,R3,#-1		;acts as the check of the last value	
-			BRp LOOP1			;checks the next value		
+			BRzp STOP		;if it is 1, we go to halt straight
+
+LOOP1			ADD R1,R1,#1		;R1 points to the next value
+			ST R1,STORE		;store the memory location of new R1			
+			AND R4,R4,#0		;clear R4
+			LDI R4,STORE		;load value in memory of R1	
+			BRp STOP		;we check the value in R4	
+			
+			ADD R3,R3,#-1		;decrement counter			
+			BRp LOOP1		;exit loop when counter goes to zero
 ;if all are checked, we load R0 with 0 and halt
 
 ;--------------------------------------------------------------------------------------------
 			AND R0,R0,#0		;load R0 with 0 when all the values are 0	
+			BRnzp STOP1		;we load R0 with 0 and go to RET			
 ;-----------loading back all the values------------------------------------------------------
-			LD R1,SAVE_1
-			LD R2,SAVE_2
-			LD R3,SAVE_3
-			LD R4,SAVE_4
-			LD R5,SAVE_5
-			LD R7,SAVE_7
+			LD R1,SAVE_1		;
+			LD R2,SAVE_2		;
+			LD R3,SAVE_3		;
+			LD R4,SAVE_4		;
+			LD R5,SAVE_5		;
+			LD R7,SAVE_7		;
 ;---------------------------------------------------------------------------------------------
-STOP		RET
+STOP		ADD R0,R0,#1			;				
+STOP1		RET
 ;------------------------------------------------------------------------------------
 GAMEBOARD	.FILL x3600
 val_one		.FILL x0031
@@ -69,4 +78,5 @@ SAVE_3		.BLKW 1
 SAVE_4		.BLKW 1
 SAVE_5		.BLKW 1
 SAVE_7		.BLKW 1
+STORE		.BLKW 1
 .END
