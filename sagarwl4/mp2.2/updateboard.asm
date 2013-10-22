0b85a6a7511e676fb69b5e3e1b8db454672291fb
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-10-22 15:57:19 +0000

    mp2.2 doing

--- sagarwl4/mp2.2/updateboard.asm
+++ sagarwl4/mp2.2/updateboard.asm
@@ -29,7 +29,7 @@
 			ADD R2,R6,#-1		;loop to get (row-1)*size
 DO_ROW			ADD R3,R3,R7		;		
 			ADD R2,R2,#-1		;
-			BRp DO_ROW			;loop ends
+			BRp DO_ROW		;loop ends
 			ADD R1,R1,R3
 			
 			AND R2,R2,#0		;	
@@ -37,7 +37,6 @@ DO_ROW			ADD R3,R3,R7		;
 		
 			ADD R1,R1,R2		;R1 now has (row-1)*size+(column-1)+GAMEBOARD
 ;--------------------------------------------------------------------------
-		
 			ST R1,GAME_SAVE		;GAMESAVE HAS THE MEMORY LOCATION OF THE ORIGINAL VALUE
 			LDI R1, GAME_SAVE	;
 			ADD R1,R1,#-15		;
@@ -55,21 +54,22 @@ DO_ROW			ADD R3,R3,R7		;
 			BRn CHANGE_1_M		;
 
 CHANGE_0_M		LD R1,val_zero		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-3		;
+			ADD R1,R1,#-1		;
 			STI R1,GAME_SAVE	;		
-			BRnzp DONE 			;
+			BRnzp DONE 		;
 
 CHANGE_1_M		LD R1,val_one		;
-			ADD R1,R1,#-1		;
+			ADD R1,R1,#1		;
 			STI R1,GAME_SAVE	;			
-			BRnzp DONE			;
-;		
-;---------changing (col+1)-----------------------------------------------
-;
+			BRnzp DONE		;
 
+;---------changing (col+1)-----------------------------------------------
+			AND R7,R7,#0		;
+			NOT R7,R4		;
+			ADD R7,R7,#1		;
+			ADD R7,R7,R6
+			BRnz DONE_Q		;if it is the last column, skip this step 				 
+;------------------------------------------------------------------------
 DONE			AND R1,R1,#0		;		
 			AND R2,R2,#0		;
 			AND R3,R3,#0		;
@@ -88,24 +88,21 @@ DONE			AND R1,R1,#0		;
 			BRzp CHANGE_0_M_Q	;
 			BRn CHANGE_1_M_Q	;
 
-CHANGE_0_M_Q	LD R1,val_zero		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-3		;
+CHANGE_0_M_Q		LD R1,val_zero		;
+			ADD R1,R1,#-1		;
 			STI R1,GAME_SAVE_1	;		
 			BRnzp DONE_Q		;
 
 CHANGE_1_M_Q		LD R1,val_one		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-3		;
+			ADD R1,R1,#1		;
 			STI R1,GAME_SAVE_1	;				
 			BRnzp DONE_Q		;
-		
 ;---------changing (col-1)-----------------------------------------------
-DONE_Q			AND R1,R1,#0		;we start decrementing column from here
+DONE_Q			AND R7,R7,#0		;
+			ADD R7,R4,#-1
+			BRnz DONE_R		;if it is the first column, skip this step
+;------------------------------------------------------------------------
+			AND R1,R1,#0		;we start decrementing column from here
 			AND R2,R2,#0
 			AND R3,R3,#0
 
@@ -123,27 +120,23 @@ DONE_Q			AND R1,R1,#0		;we start decrementing column from here
 			BRzp CHANGE_0_M_R	;
 			BRn CHANGE_1_M_R	;
 
-CHANGE_0_M_R	LD R1,val_zero		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-3		;	
+CHANGE_0_M_R		LD R1,val_zero		;
+			ADD R1,R1,#-1		;
 			STI R1,GAME_SAVE_2	;	
 			BRnzp DONE_R		;
 	
-CHANGE_1_M_R	LD R1,val_one		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-3		;
+CHANGE_1_M_R		LD R1,val_one		;
+			ADD R1,R1,#1		;
 			STI R1,GAME_SAVE_2	;		
 			BRnzp DONE_R		;
-		
 ;---------changing (row-1)-----------------------------------------------------------
-
+DONE_R			AND R7,R7,#0		;
+			ADD R7,R5,#-1
+			BRnz DONE_J		;if it is the first row, skip this step
+;------------------------------------------------------------------------------------
 ;here we will have to change the formula we initially loaded into R1
 
-DONE_R			AND R1,R1,#0
+			AND R1,R1,#0
 			AND R2,R2,#0
 			AND R3,R3,#0
 			AND R7,R7,#0
@@ -154,7 +147,7 @@ DONE_R			AND R1,R1,#0
 			ADD R1,R1,R7		;now R1 has GAMEBOARD+(row-2)*size+(column-1)			
 						
 			ST R1,GAME_SAVE_3	;GAMESAVE HAS THE MEMORY LOCATION OF THE ORIGINAL VALUE
-			LDI R1, GAME_SAVE_3
+			LDI R1,GAME_SAVE_3
 			ADD R1,R1,#-15		;
 			ADD R1,R1,#-15		;
 			ADD R1,R1,#-15		;
@@ -181,32 +174,36 @@ DO_ROW_K		ADD R3,R3,R7		;
 			BRn CHANGE_1_M_K	;
 
 CHANGE_0_M_J		LD R1,val_zero		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-3		;		
+			ADD R1,R1,#-1		;
 			STI R1,GAME_SAVE_3	;
 			BRnzp DONE_J		;
 
 CHANGE_1_M_K		LD R1,val_one		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-3		;		
+			ADD R1,R1,#1		;
 			STI R1,GAME_SAVE_3	;
 			BRnzp DONE_J		;
 		
 ;---------changing (row+1)-----------------------------------------------------------
+DONE_J			AND R7,R7,#0		;
+			NOT R7,R5		;
+			ADD R7,R7,#1		;
+			ADD R7,R7,R6
+			BRnz DONE_Z		;if it is the last row, skip this step 
+;------------------------------------------------------------------------------------
 ;here we will have to change the formula we initially loaded into R1
 
-DONE_J			AND R1,R1,#0
+			AND R1,R1,#0
 			AND R2,R2,#0
 			AND R3,R3,#0
+			AND R7,R7,#0
 
-			LD R1,INITIAL		;load R1 with GAMEBOARD
+			LD R1,GAME_SAVE		;load R1 with GAMEBOARD
+			ADD R7,R7,R6				
+			ADD R1,R1,R7			
+			
 			ADD R3,R5,#0		;R0W
 			ST R1,GAME_SAVE_4	;GAMESAVE HAS THE MEMORY LOCATION OF THE ORIGINAL VALUE
-			LDI R1, GAME_SAVE_4
+			LDI R1,GAME_SAVE_4
 			ADD R1,R1,#-15		;
 			ADD R1,R1,#-15		;
 			ADD R1,R1,#-15		;
@@ -237,23 +234,22 @@ DO_ROW_I		ADD R3,R3,#0		;
 			BRzp CHANGE_0_M_Z	;
 			BRn CHANGE_1_M_Z	;
 
-CHANGE_0_M_Z	LD R1,val_zero		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-3		;		
+CHANGE_0_M_Z		LD R1,val_zero		;
+			ADD R1,R1,#-1		;
 			STI R1,GAME_SAVE_4	;
 			BRnzp DONE_Z		;
 
-CHANGE_1_M_Z	LD R1,val_one	;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-15		;
-			ADD R1,R1,#-3		;		
+CHANGE_1_M_Z		LD R1,val_one		;
+			ADD R1,R1,#1		;
 			STI R1,GAME_SAVE_4	;
 			BRnzp DONE_Z		;
 		
-DONE_Z			RET
+DONE_Z			LD R1,SAVE_1		;
+			LD R2,SAVE_2		;
+			LD R3,SAVE_3		;
+			LD R7,SAVE_7		;
+
+			RET
 ;-------------------------------------------------------------------------------------------------
 
 val_zero		.FILL x0030
@@ -268,16 +264,5 @@ GAME_SAVE_1		.BLKW 1
 GAME_SAVE_2		.BLKW 1
 GAME_SAVE_3		.BLKW 1
 GAME_SAVE_4		.BLKW 1
-ZIP_1			.BLKW 1
-ZIP_2			.BLKW 1
-ZIP_3			.BLKW 1
-ZIP_4			.BLKW 1
-ZIP_5			.BLKW 1
-ZIP_6			.BLKW 1
-ZIP_7			.BLKW 1
-ZIP_8			.BLKW 1
-ZIP_9			.BLKW 1
-ZIP_10			.BLKW 1
 FINAL			.BLKW 1
 .END
-
