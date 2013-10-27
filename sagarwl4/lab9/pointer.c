6008eadfce9b351cace2caea66f2fef96e0e15fd
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-10-27 05:16:11 +0000

    lab9 doing

--- sagarwl4/lab9/pointer.c
+++ sagarwl4/lab9/pointer.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
-
+ 
 int main()
 {
-   int iray[5];
-   int* ipoint = &iray[0];
-   int i;
-   for(i = 0; i < 100; i++)
-   {
-      *ipoint = i;
-       printf("%p\n", ipoint);
-       ipoint = ipoint + 1;
-   }
-
-   for(i = 0; i < 5; i++)
-   {
-      printf("%d\n", iray[i]);
-   }
-   return 0;
+    int iray[5];
+    int* ipoint = &iray[0];
+ 
+    for(int i = 0; i < 100; i++)
+    {
+        *ipoint = i;
+        printf("%p\n", ipoint);
+        ipoint = ipoint + 1;
+    }
+ 
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%d\n", iray[i]);
+    }
+    return 0;
 }
-
