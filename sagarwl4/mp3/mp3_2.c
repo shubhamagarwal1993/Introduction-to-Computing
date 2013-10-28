1d8aef51e71920efe16e51eb30cacae594d490c8
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-10-28 21:12:30 +0000

    mp3 is going gr8

--- sagarwl4/mp3/mp3_2.c
+++ sagarwl4/mp3/mp3_2.c
@@ -8,52 +8,51 @@
 
 float polynomial(float A, float B, float C, float x);
 
-float riemann(float a, float b, int N, float A, float B, float C, char method);
+float riemann(float a, float b, int N, float A, float B, float C);
 /*
     Document functions (and the rest of your code) according to the style guide
 */
 
-int main() {
-    /* mp3.2 code here */
-    	float a;				/*starting point of integration*/
-	float b;				/*endpoint of integration*/
-	float A;				/*coefficient of x^2*/		
-	float B;				/*coeficient of x*/
-	float C;				/*constant*/
-	float N;				/*the number of rectangles*/
-	float left;				/*left method sum*/
-	float right;				/*right method sum*/
-	float middle;				/*middle method sum*/
-	float average;				/*average sum*/
-
+int main() 
+{
+	float a;					/*starting point of integration*/
+	float b;					/*endpoint of integration*/
+	float A;					/*coefficient of x^2*/		
+	float B;					/*coeficient of x*/
+	float C;					/*constant*/
+	float N;					/*the number of rectangles*/
+	float left;					/*left method sum*/
+	/*(float right;	*/			/*right method sum*/
+	/*float middle;*/				/*middle method sum*/
+	/*float average;*/				/*average sum*/
+	/*int L,M,R;
+	L=left;
+	M=middle;
+	R=right;*/
+	
 	printf("\n");	
 	printf("integration of Ax^2+Bx+C over a<=x<=b with N rectangles!\n");
 	printf("\n");
+	
 	printf("enter startpoint\n");
 	scanf("%f", &a);	
-		
 LOOP1:
 	printf("enter endpoint\n");
 	scanf("%f", &b);
-	
 	if (a>b)
 	{
 		goto LOOP1;		
 	}
-
 	printf("enter value for A\n");
 	scanf("%f", &A);
-
 	printf("enter value for B\n");
 	scanf("%f", &B);
-
 	printf("enter value for C\n");
 	scanf("%f", &C);
 
 LOOP2:	
 	printf("enter the number of rectangles you want\n");
 	scanf("%f", &N);
-	
 	if (N<=0)
 	{
 		goto LOOP2;	
@@ -66,44 +65,39 @@ LOOP2:
 	printf("Input b: %f \n", b);	
 	printf("Results: \n");
 	printf("%fx^2 + %fx + %f \n",A,B,C);
+	
 	left = riemann(a,b,N,A,B,C);
 	printf("left method sum = %f \n", left);
-	printf("right method sum = %f \n", right);
-	printf("middle method sum = %f \n", middle);
-	printf("average sum = %f \n", average);
-    
+	
     
     return 0;
-
 }
 
-float polynomial(float A, float B, float C, float x)
+/*float polynomial(float A, float B, float C, float x)
 {
 	int sum;
 	sum = (A*x*x)+(B*x)+(C);		 	
 	return sum;
-
 }
-	
-float riemann(float a, float b, int N, float A, float B, float C /*,char method*/)
+*/	
+float riemann(float a, float b, int N, float A, float B, float C)
 {	
-	int L, R, M;
-	int del_x;
-	int sum_L,sum_R,sum_M;
+	/*int L, R, M;*/
+	float del_x;
+	float sum_L/*,sum_R,sum_M*/;
 	del_x = (b-a)/N;
-	int sum_riemann;
- 	int x;
+	/*int sum_riemann;*/
+ 	float x;
 
-	if (method == L)	
-	{
-		for (x = a; x < (N*del_x); x=x+del_x)
+	
+		for (x = a; x < b; x=x+del_x)
 		{ 	
-			sum_L = sum_L + (A*x*x)+(B*x)+(C);
+			sum_L = sum_L + (((A*x*x)+(B*x)+(C))*del_x);
 		}			
 		return sum_L;	
-	}
-
-	if (mehod == R)
+	
+/*
+	if (method == R)
 	{
 		for (x = a+del_x; x <= b; x=x+del_x)
 		{ 	
@@ -119,8 +113,9 @@ float riemann(float a, float b, int N, float A, float B, float C /*,char method*
 		{ 	
 			sum_M = sum_M + (A*x*x)+(B*x)+(C);
 		}			
-		return sum_M;	
-
-	}
+		return sum_M;
+		}	
+*/
+	
 
 }	
