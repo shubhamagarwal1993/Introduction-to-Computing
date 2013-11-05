4d49c6e0c9accacb9d1760947584e5cafb2ab9eb
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-11-05 17:08:06 +0000

    doing mp4

---
+++ mp4/problem5/problem5.c
@@ -0,0 +1,21 @@
+/*finding the approximate value of e^x*/
+
+#include<stdio.h>
+float Exp(float x, float n);
+int main()
+{
+	float x;
+	float n;
+	float result;
+	printf("input x and n: ");
+	scanf("%f %f ", &x, &n);
+	
+	printf("exp(%f) = %f\n",x,result); 
+} 
+
+float Exp()
+{
+	float sum;
+
+	return sum;
+}
