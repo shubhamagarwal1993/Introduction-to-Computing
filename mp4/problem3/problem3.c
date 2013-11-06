71a50dc9ae27ababe06a451a519d69ce140ac42e
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-11-06 20:07:29 +0000

    fdfgg

--- mp4/problem3/problem3.c
+++ mp4/problem3/problem3.c
@@ -1,26 +1,26 @@
 /* computing the minimum and maximum elements of an array*/
 
 #include<stdio.h>
-#define N 10
+#define N 10												//global defination
 
 int setMinMax(int List[], char method);
 
 int main()
 {
 	int List[N]={4,5,10,19,-43,-3,19,33,34,2};
-	int maxInList = setMinMax(List,'a');
-	int minInList = setMinMax(List,'b');
+	int maxInList = setMinMax(List,'a');					//calling max
+	int minInList = setMinMax(List,'b');					//calling min
 	printf("Min %d Max %d \n", minInList, maxInList );
-	return 0;
+	return 0;												//in case nothing satisfies
 }
 
-int setMinMax(int List[], char method)
+int setMinMax(int List[], char method)						//one function for min and max	
 {
 	int i;
 	int maxInList;
 	int minInList;
 	/*for finding max number*/
-	if (method == 'a')
+	if (method == 'a')										//finding max
 	{
 		maxInList = List[0];
 		for (i=1; i<10; i++)
@@ -33,7 +33,7 @@ int setMinMax(int List[], char method)
 	return maxInList;
 	}
 	/*for finding min number*/
-	if (method == 'b')
+	if (method == 'b')										//finding min
 	{
 		minInList = List[0];
 		for (i=1; i<10; i++)
