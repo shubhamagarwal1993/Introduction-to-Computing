680bff6a11b635dc5bc162293bc02d5750974192
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-11-06 05:56:17 +0000

    mp4 doinmg

--- mp4/problem5/problem5.c
+++ mp4/problem5/problem5.c
@@ -1,21 +1,53 @@
 /*finding the approximate value of e^x*/
 
 #include<stdio.h>
-float Exp(float x, float n);
+float Exp(float a, float b);
+float factorial(float n); 
+float raisedto(float j, float k);
+
+
 int main()
 {
 	float x;
-	float n;
-	float result;
+	int n;
 	printf("input x and n: ");
-	scanf("%f %f ", &x, &n);
+	scanf("%f %d ", &x, &n);
 	
-	printf("exp(%f) = %f\n",x,result); 
+	printf("%f",Exp(x,n));
+	return 0;
 } 
 
-float Exp()
-{
-	float sum;
 
+float factorial(float n)
+{
+	float c;
+	float fact = 1;
+	for (c = 1; c <=n; c++)
+	{
+		fact = fact*c;
+	}
+	return fact;
+}
+
+float raisedto(float j, float k)
+{
+	int i = 0; 
+	int pro = 1;
+	for (i=0; i < k; i++)
+	{
+		pro = pro*j;
+	}
+	return pro;
+} 
+float Exp(float a, float b)
+{
+	float sum = 0;
+	int i;	
+	for (i = 1; i <= b; i++)
+		{
+			sum = sum + (raisedto(a,i)/factorial(i));
+		}	
+	sum = 1 + sum; 
 	return sum;
 }
+
