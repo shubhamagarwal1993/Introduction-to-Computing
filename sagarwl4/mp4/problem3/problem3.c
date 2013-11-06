0156feb1c3ff751e521f8054426572d2bd104517
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-11-06 22:32:21 +0000

    mp4 done

--- sagarwl4/mp4/problem3/problem3.c
+++ sagarwl4/mp4/problem3/problem3.c
@@ -3,47 +3,37 @@
 #include<stdio.h>
 #define N 10												//global defination
 
-int setMinMax(int List[], char method);
+int setMinMax(int List[], *minInList, *maxInList );
 
 int main()
 {
 	int List[N]={4,5,10,19,-43,-3,19,33,34,2};
-	int maxInList = setMinMax(List,'a');					//calling max
-	int minInList = setMinMax(List,'b');					//calling min
+	int maxInList;									//declaring max
+	int minInList;									//declaring min
+	setMinMax(List, &minInList, &maxInList);		//calling the function
 	printf("Min %d Max %d \n", minInList, maxInList );
 	return 0;												//in case nothing satisfies
 }
 
-int setMinMax(int List[], char method)						//one function for min and max	
+int setMinMax(int List[], *minInList, *maxInList )		//one function for min and max	
 {
 	int i;
-	int maxInList;
-	int minInList;
-	/*for finding max number*/
-	if (method == 'a')										//finding max
+	int maximum = List[0];
+	int minimum = List[0];
+	
+	for (i = 1; i < N; i++)
 	{
-		maxInList = List[0];
-		for (i=1; i<10; i++)
+					//finding max
+		if (List[i] > maxInList)
 		{
-			if (List[i] > maxInList)
-			{
-				maxInList = List[i];
-			}
+			maximum = List[i];
 		}
-	return maxInList;
-	}
-	/*for finding min number*/
-	if (method == 'b')										//finding min
-	{
-		minInList = List[0];
-		for (i=1; i<10; i++)
+					//finding min
+		if (List[i] < minInList)
 		{
-			if (List[i] < minInList)
-			{
-				minInList = List[i];
-			}
+			minimum = List[i];
 		}
-	return minInList;
 	}
-return 0;
+*minInList = minimum;
+*maxInList = maximum;
 }
