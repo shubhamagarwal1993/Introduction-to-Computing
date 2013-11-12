5e1d78fe1ff13476f2dc5bcd0f6905a0bb8c487e
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-11-12 04:19:51 +0000

    fer

---
+++ test2_prac/problem15/length.c
@@ -0,0 +1,24 @@
+/* text compression using RLE method */
+
+#include <stdio.h>
+#define N 128
+
+int str_len(char *input);
+
+int main()
+{
+	int len;
+	char input[N];
+	printf("enter text: ");
+	scanf("%s", input);
+	len = str_len(input);
+	printf("length = %d\n", len);
+	return 0;
+}
+
+int str_len(char *input)
+{
+	int i = 0;
+	while (input[i++] != '\0');
+	return i-1;
+}
