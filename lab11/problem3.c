bb33b8f07f5539e2c1c80340b8c18c034e16593d
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-11-12 05:20:59 +0000

    fre

--- lab11/problem3.c
+++ lab11/problem3.c
@@ -3,22 +3,43 @@
 #include<stdio.h>
 #define N 128
 
-//int reverse(char in[], char out[]);
+void reverse(char result[]);
+int str_length(char array[]);
+void char_swap(char *firstVal, char *secondVal);
 
 int main()
 {
-char result[128];
-printf("enter text");
-
-scanf("%128s",result);
-//reverse();
-printf("%s",result);
-
+char input[N];
+printf("enter text: ");
+scanf("%s", input);
+reverse(input);
+printf("%s", input);
+return 0;
 }
 
-//int reverse(char in[], char out[])
-//{
+int str_length(char array[]){
+	int i = 0;
+	int count = 0;
+	while (array[i++] != '\0'){
+	count++;
+	}
+	return count;
+}
 
-//}
+void char_swap(char *firstVal, char *secondVal){
+	char tempVal;
+	tempVal = *firstVal;
+	*firstVal = *secondVal;
+	*secondVal = tempVal;
+}
+
+void reverse(char result[]){
+	int leng = str_length(result);	
+	for (int i = 0; i < (leng/2); i++)
+	{
+		char_swap(&result[i], &result[leng-i-1]);
+	}
+	
+}
 
 
