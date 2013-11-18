53cd5a1d19370d92caa9914c04ca2fc7011dd396
sagarwl4 <sagarwl4@c1ce2291-aabf-4510-9021-15b849677880>
2013-11-18 22:45:35 +0000

    done

--- sagarwl4/lab12/llist2.c
+++ sagarwl4/lab12/llist2.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
 typedef struct node_t
 {
     int data;
     struct node_t *next;
 } node;
-
 void reverse(node** head);
-
 // Fucnction to append nodes to the Linked list.
 void append(node **head,int data)
 {   
 		
     node *temp,*n;
-
     temp = (node *)malloc(sizeof(node));
     temp->data=data;
     temp->next=NULL;
-
     n = *head;
-
     if(*head==NULL)
     {   
         *head=temp;
@@ -36,8 +29,6 @@ void append(node **head,int data)
 	n->next=temp;
     }
 }
-
-
 void printList(node *head)
 {
     node *n = head;
@@ -49,12 +40,9 @@ void printList(node *head)
     }
     printf("\n");
 }
-
-
 int main()
 {
     node* head = NULL;
-
     append(&head,20);
     append(&head,10);
     append(&head,90);
@@ -64,26 +52,37 @@ int main()
     append(&head,4);
     append(&head,60);
     append(&head,05);
-
     printf("Before:\n");
     printList(head);
-
     //call your function here
-
-
+	reverse(&head);
     printf("After:\n");
     printList(head);
-
     //free linked list here
-
-
+	while(head!=NULL)
+	{
+		node* temp = head;
+		head=head->next;
+		free(temp);
+	}
     return 0;
 }
-
 void reverse(node** head)
 {
     //add your code here
-
-
+    if(*head==NULL)
+    	return;
+	node* h=*head;
+	node* prev=NULL;
+	node* temp=h->next;
+	while(h!=NULL)
+	{
+		h->next=prev;
+		prev=h;
+		if(temp==NULL)
+			break;
+		h=temp;
+		temp=temp->next;
+	}
+	*head=h;
 }
-
